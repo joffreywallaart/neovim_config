4c6935c791f4ac8180f831abd4f116dacfda046a
Joffrey Wallaart <jwallaart@tudelft.nl>
2024-01-23 14:07:24 +0100

    keybinds

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
 
